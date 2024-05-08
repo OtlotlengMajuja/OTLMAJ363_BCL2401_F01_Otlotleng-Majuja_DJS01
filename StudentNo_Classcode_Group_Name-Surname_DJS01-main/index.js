0e04a2e6b46752752cf0b3bf7ba8f3a5cfa49013
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-08 09:52:12 +0200

    Corrected the calculation for the distance. Calculated the remaining fuel by subtracting the product of the fuel burning rate and the time. Declared acc2 variable to convert the unit.

--- StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
@@ -15,20 +15,23 @@ const fbr = 0.5; // fuel burn rate (kg/s)
 // Conversion factor for acceleration
 const conversionFactor = { m_s2_to_kmh: 3.6 }; // 1 m/s^2 ≈ 3.6 km/h
 
-const distance2 = distance + (velocity(time * conversionFactor)); //calculates new distance
+// Convert the time to hours for consistent unit
+const distance2 = distance + (velocity * (time / 3600)); //Calculates new distance
 
-
-const rf = fbr * time //calculates remaining fuel
-const vel2 = calcNewVel(velocity, acceleration, time) //calculates new velocity based on acceleration
+// Calulate the remaining fuel
+const rf = fuel - (fbr * time); //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
 calcNewVel = (velocity, acceleration, time) => {
-  return velocity + (acceleration * time)
+  const acc2 = acceleration * conversionFactor;
+  return velocity + (acc2 * time)
 }
 
-//console.log(`Corrected New Velocity: ${vel2} km/h`);
+const vel2 = calcNewVel(velocity, acceleration, time);  //calculates new velocity based on acceleration
+
+console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
-//console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected Remaining Fuel: ${rf} kg`);
 
 
 
