0b85057957758de065a8a795012aecfcfdad0326
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-08 09:17:00 +0200

    Renamed the variables

--- StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
@@ -6,21 +6,23 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const velocity = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
+const distance = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel * time) //calcultes new distance
+const distance2 = distance + (velocity * time) //calcultes new distance
+
+
 const rf = fbr * time //calculates remaining fuel
-const vel2 = calcNewVel(vel, acc, time) //calculates new velocity based on acceleration
+const vel2 = calcNewVel(velocity, acceleration, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => {
-  return vel + (acc * time)
+calcNewVel = (velocity, acceleration, time) => {
+  return velocity + (acceleration * time)
 }
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
